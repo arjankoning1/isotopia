f27bc3de5a035f7dbeb803465be4c37a3e830e4c
Arjan Koning <arjankoning1@gmail.com>
2024-12-25 12:27:08 +0100

    update

--- source/inputout.f90
+++ source/inputout.f90
@@ -5,7 +5,7 @@ subroutine inputout
 !
 ! Revision    Date      Author      Quality  Description
 ! ======================================================
-!    1     2023-12-29   A.J. Koning    A     Original code
+!    1     2024-12-29   A.J. Koning    A     Original code
 !-----------------------------------------------------------------------------------------------------------------------------------
 !
 ! *** Use data from other modules
@@ -42,9 +42,9 @@ subroutine inputout
 !
 ! *************************** Code and version *************************
 !
-  write(*, '(/"    ISOTOPIA-2.0    (Version: December 29, 2023)"/)')
-  write(*, '(10x, " Prediction of medical isotope production with ", "accelerators")')
-  write(*, '(/" Copyright (C) 2023  A.J. Koning")')
+  write(*, '(/"    ISOTOPIA-2.1    (Version: December 29, 2024)"/)')
+  write(*, '(10x, " Prediction of medical isotope production with accelerators")')
+  write(*, '(/" Copyright (C) 2024  A.J. Koning")')
   write(*, '(/" User: ",a)') trim(user)
   write(*, '(" Date: ",a)') trim(date)
 !
@@ -59,7 +59,7 @@ subroutine inputout
 ! ********* All possible input parameters including defaults ***********
 !
   write(*, '(/" USER INPUT FILE + DEFAULTS"/)')
-  write(*, '(" Keyword           Value   Variable", "     Explanation"/)')
+  write(*, '(" Keyword           Value   Variable     Explanation"/)')
   write(*, '(" user      ", a, t28, "user         user for this calculation")') trim(user)
   write(*, '(" source    ", a, t28, "source       source for this calculation")') trim(source)
   write(*, '(" format    ", a, t28, "oformat      format for output")') trim(oformat)
@@ -67,37 +67,37 @@ subroutine inputout
 ! 1. Nuclear reaction
 !
   write(*, '(" #"/" # Nuclear reaction"/" #")')
-  write(*, '(" projectile          ", a1, "     ptype0", "       type of incident particle")') ptype0
-  write(*, '(" element            ", a2, "     Starget", "      symbol of target nucleus")') Starget
-  write(*, '(" mass              ", i3, "     Atarget  ", "    mass number of target nucleus")') Atarget
+  write(*, '(" projectile          ", a1, "     ptype0       type of incident particle")') ptype0
+  write(*, '(" element            ", a2, "     Starget      symbol of target nucleus")') Starget
+  write(*, '(" mass              ", i3, "     Atarget      mass number of target nucleus")') Atarget
 !
 ! 2. Accelerator
 !
   write(*, '(" #"/" # Accelerator "/" #")')
-  write(*, '(" Ebeam             ", f7.3, " Ebeam ", "       incident energy in MeV")') Ebeam
-  write(*, '(" Eback             ", f7.3, " Eback ", "       lower end of energy range in MeV")') Eback
-  write(*, '(" Ibeam             ", f7.3, " Ibeam ", "       beam current in mA")') Ibeam
+  write(*, '(" Ebeam             ", f7.3, " Ebeam        incident energy in MeV")') Ebeam
+  write(*, '(" Eback             ", f7.3, " Eback        lower end of energy range in MeV")') Eback
+  write(*, '(" Ibeam             ", f7.3, " Ibeam        beam current in mA")') Ibeam
   do i = 1, 5
     if (Tirrad(i) > 0) write(*, '(" Tirrad      ", i9, "     Tirrad       ", a1, " of irradiation time")') Tirrad(i), unitTirrad(i)
   enddo
-  write(*, '(" radiounit             ", a3, " radiounit ", "   unit for radioactivity")') radiounit
-  write(*, '(" yieldunit             ", a3, " yieldunit ", "   unit for isotope yield")') yieldunit
+  write(*, '(" radiounit             ", a3, " radiounit    unit for radioactivity")') radiounit
+  write(*, '(" yieldunit             ", a3, " yieldunit    unit for isotope yield")') yieldunit
 !
 ! 3. Target
 !
   write(*, '(" #"/" # Target"/" #")')
-  write(*, '(" Area              ", f7.3, " Area  ", "       target area in cm^2")') Area
+  write(*, '(" Area              ", f7.3, " Area         target area in cm^2")') Area
   do i = 1, 5
     if (Tcool(i) > 0) write(*, '(" Tcool       ", i9, "     Tcool        ", a1, " of cooling time")') Tcool(i), unitTcool(i)
   enddo
-  write(*, '(" rho               ", f7.3, " rhotarget   ", " target density [g/cm^3] ")') rhotarget
+  write(*, '(" rho               ", f7.3, " rhotarget    target density [g/cm^3] ")') rhotarget
 !
 ! 4. Cross section and decay data
 !
   write(*, '(" #"/" # Cross section and decay data"/" #")')
-  write(*, '(" Zdepth            ", i3, "     Zdepth   ", "    depth to which Z numbers are scanned for cross sections")') Zdepth
-  write(*, '(" Adepth            ", i3, "     Adepth   ", "    depth to which A numbers are scanned for cross sections")') Adepth
+  write(*, '(" Zdepth            ", i3, "     Zdepth       depth to which Z numbers are scanned for cross sections")') Zdepth
+  write(*, '(" Adepth            ", i3, "     Adepth       depth to which A numbers are scanned for cross sections")') Adepth
   write( * , * )
   return
 end subroutine inputout
-! Copyright A.J. Koning 2023
+! Copyright A.J. Koning 2024
