ad82428896ddb110006283cb31017f362c4b6c50
Arjan Koning <arjankoning1@gmail.com>
2026-03-17 13:14:56 +0100

    add neutrons and photons

--- source/prodrates.f90
+++ source/prodrates.f90
@@ -5,7 +5,7 @@ subroutine prodrates
 !
 ! Revision    Date      Author      Quality  Description
 ! ======================================================
-!    1     2023-02-25   A.J. Koning    A     Original code
+!    1     2026-03-09   A.J. Koning    A     Original code
 !-----------------------------------------------------------------------------------------------------------------------------------
 !
 ! *** Use data from other modules
@@ -36,37 +36,96 @@ subroutine prodrates
 ! *** Declaration of local data
 !
   implicit none
-  integer, parameter :: numint=100       ! number of integration points
+  character(len=3)   :: Estring          ! string
+  character(len=132) :: string           ! line
+  character(len=132) :: specfile        ! 
+  integer, parameter :: numint=10000     ! number of integration points
   integer            :: ia               ! mass number from abundance table
   integer            :: is               ! isotope counter: -1=total, 0=ground state 1=isomer
   integer            :: iz               ! charge number of residual nucleus
   integer            :: N                ! neutron number of residual nucleus
   integer            :: nE               ! number of energies
   integer            :: nen              ! energy counter
-  integer            :: Ninte            ! number of integration points
+  integer            :: iE
+  integer            :: istat
+  integer            :: NintE            ! number of integration points
   real(sgl)          :: dE               ! help variable
   real(sgl)          :: dEdx             ! stopping power
   real(sgl)          :: dxdE(numint)     ! 1/stopping power
+  real(sgl)          :: phi(numint)      ! spectrum
   real(sgl)          :: dxdEsum          ! integration sum
   real(sgl)          :: E                ! incident energy
   real(sgl)          :: Ea               ! start energy of local adjustment
   real(sgl)          :: Eb               ! end energy of local adjustment
   real(sgl)          :: Egrid(0:numen)   !
-  real(sgl)          :: Eint(numint)     ! energy on integration grid
+  real(sgl)          :: dum
+  real(sgl)          :: Eint(0:numint)   ! energy on integration grid
+  real(sgl)          :: dEint(numint)    ! delta energy on integration grid
   real(sgl)          :: ratesum          ! integration sum
   real(sgl)          :: xs               ! help variable
   real(sgl)          :: xsa              ! help variable
   real(sgl)          :: xsb              ! help variable
 !
-! *********** Determine integration grid and stopping power ************
+! **** Photons: read Bremsstrahlung spectrum for incident electron energy
+!
+  dEint = 0.
+  Eint = 0.
+  phi = 0.
+  if (k0 == 0) then
+    iE = min(200, int(Ebeam))
+    iE = max(iE, 1)
+    Estring = '000'
+    write(Estring(1:3), '(i3.3)') iE
+    specfile = trim(path)//'files/photon_spectra/brems_'//Estring//'MeV.txt'
+    open (unit = 1, status = 'unknown', file = specfile)
+    nen = 0
+    do
+      read(1, '(a)', iostat = istat) string
+      if (istat == -1) exit
+      if (string(1:1) == '#') cycle
+      nen = nen + 1
+      read(string, *) Eint(nen), phi(nen)
+    enddo
+    close(1)
+    NintE = nen
+    do nen = 2, nintE
+      dEint(nen-1) = Eint(nen) - Eint(nen-1)
+    enddo
+    dEint(NintE) = dEint(NintE-1)
+  endif
+!
+! **** Neutrons: read neutron spectrum 
+!
+  if (k0 == 1) then
+    iE = min(200, int(Ebeam))
+    iE = max(iE, 1)
+    Estring = '000'
+    write(Estring(1:3), '(i3.3)') iE
+    specfile = trim(path)//'files/neutron_spectra/n-hfr.dat'
+    open (unit = 1, status = 'unknown', file = specfile)
+    nen = 0
+    do
+      read(1, '(a)', iostat = istat) string
+      if (istat == -1) exit
+      if (string(1:1) == '#') cycle
+      nen = nen + 1
+      read(string, *) iE, Ea, Eb, dum,  phi(nen)
+      Eint(nen) = 0.5 * (Ea + Eb) * 1.e-6
+      dEint(nen) = (Ea - Eb) * 1.e-6
+    enddo
+    close(1)
+    NintE = nen
+  endif
+!
+! **** Charged particles: Determine integration grid and stopping power *
 !
 ! stoppingpower: subroutine to calculate stopping power
 !
-  if ( k0 > 1) then
-    Ninte = 100
-    dE = (Ebeam - Eback) / Ninte
+  if (k0 > 1) then
+    NintE = 100
+    dE = (Ebeam - Eback) / NintE
     dxdEsum = 0.
-    do nE = 1, Ninte
+    do nE = 1, NintE
       Eint(nE) = Eback + (nE - 0.5) * dE
       call stoppingpower(Eint(nE), dEdx)
       dxdE(nE) = 1. / dEdx
@@ -77,6 +136,8 @@ subroutine prodrates
     Mtar = rhotarget * Vtar
     heat = Ibeam * (Ebeam - Eback)
     projnum = Ibeam / (1000. * parZ(k0) * qelem)
+  else
+    Mtar = targetmass
   endif
 !
 ! ********************* Calculate reaction rates ***********************
@@ -103,7 +164,7 @@ subroutine prodrates
         do nen = 1, N
           Egrid(nen) = Ein(nen)
         enddo
-        do nE = 1, Ninte
+        do nE = 1, NintE
           E = Eint(nE)
           if (E < Egrid(1)) cycle
           if (E > Egrid(N)) exit
@@ -114,12 +175,18 @@ subroutine prodrates
           xsa = xsrp(nen)
           xsb = xsrp(nen + 1)
           call pol1(Ea, Eb, xsa, xsb, E, xs)
-          ratesum = ratesum + dxdE(nE) * xs
+          if (k0 == 0) ratesum = ratesum + phi(nE) * xs * dEint(nE)
+          if (k0 == 1) ratesum = ratesum + phi(nE) * xs
+          if (k0 > 1) ratesum = ratesum + dxdE(nE) * xs
         enddo
-        prate(iz, ia, is) = projnum / Vtar * ratesum * dE * 1.e-27
+        if (k0 <= 1) then
+          prate(iz, ia, is) = fluxtotal  * ratesum * 1.e-27
+        else
+          prate(iz, ia, is) = projnum / Vtar * ratesum * dE * 1.e-27
+        endif
       enddo
     enddo
   enddo
   return
 end subroutine prodrates
-! Copyright A.J. Koning 2023
+! Copyright A.J. Koning 2026
